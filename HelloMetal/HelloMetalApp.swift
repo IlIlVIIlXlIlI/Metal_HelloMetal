f185df2b96acd9d106928918ae84c57fbd83f2d9
Shogo <vanitas710@yahoo.co.jp>
2021-02-13 23:49:32 +0900

    Initial Commit

---
+++ HelloMetal/HelloMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMetalApp.swift
+//  HelloMetal
+//
+//  Created by Shogo Nobuhara on 2021/02/13.
+//
+
+import SwiftUI
+
+@main
+struct HelloMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
