e7e1eca032aca923072c0eda4ccf621ba5fcc0c9
Shogo <vanitas710@yahoo.co.jp>
2021-02-13 23:56:14 +0900

    MetalViewクラスの追加

---
+++ HelloMetal/MetalView.swift
@@ -0,0 +1,8 @@
+//
+//  MetalView.swift
+//  HelloMetal
+//
+//  Created by Shogo Nobuhara on 2021/02/13.
+//
+
+import Foundation
---
+++ HelloMetal/Renderer.swift
@@ -0,0 +1,22 @@
+//
+//  Renderer.swift
+//  HelloMetal
+//
+//  Created by Shogo Nobuhara on 2021/02/13.
+//
+
+import Foundation
+import MetalKit
+
+class Renderer: NSObject,MTKViewDelegate
+{
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        <#code#>
+    }
+    
+    func draw(in view: MTKView)
+    {
+        
+    }
+}
