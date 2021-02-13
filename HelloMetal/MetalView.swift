c1866c99a51ba5ca5f6f6e27b4e174a2ac89f13e
Shogo <vanitas710@yahoo.co.jp>
2021-02-14 00:40:13 +0900

    Metalの初期化が成功した。

--- HelloMetal/MetalView.swift
+++ HelloMetal/MetalView.swift
@@ -5,4 +5,43 @@
 //  Created by Shogo Nobuhara on 2021/02/13.
 //
 
-import Foundation
+import SwiftUI
+import MetalKit
+
+struct MetalView:UIViewRepresentable {
+    
+    //MTKViewを表示する
+    typealias UIViewType = MTKView
+    
+    func makeUIView(context: Context) -> MTKView {
+        let view = MTKView()
+        view.device = MTLCreateSystemDefaultDevice()
+        view.delegate = context.coordinator
+        
+        view.clearColor = MTLClearColor(red: 0.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        if let device = view.device {
+            context.coordinator.setup(device: device)
+        }
+        
+        return view
+    }
+    
+    
+    // ビューの更新処理
+    func updateUIView(_ uiView: MTKView, context: Context) {
+        
+    }
+    
+    // コーディネーターを作る
+    func makeCoordinator() -> Renderer {
+        return Renderer(self)
+    }
+    
+}
+
+struct MetalView_Previews: PreviewProvider {
+    static var previews: some View {
+        MetalView()
+    }
+}
--- HelloMetal/Renderer.swift
+++ HelloMetal/Renderer.swift
@@ -8,15 +8,48 @@
 import Foundation
 import MetalKit
 
-class Renderer: NSObject,MTKViewDelegate
-{
+class Renderer: NSObject, MTKViewDelegate {
+    
+    // このプロパティを追加する
+    let parent: MetalView
+    
+    // コマンドキューの追加
+    var commandQueue: MTLCommandQueue?
+    
+    // イニシャライザを追加する
+    init(_ parent: MetalView) {
+        self.parent = parent
+    }
+    
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        <#code#>
+        
     }
     
     func draw(in view: MTKView)
     {
+        guard let cmdBuffer = self.commandQueue?.makeCommandBuffer()else {
+            return
+        }
         
+        guard let renderPassDesc = view.currentRenderPassDescriptor else {
+            return
+        }
+        
+        guard let encorder = cmdBuffer.makeRenderCommandEncoder(descriptor: renderPassDesc) else {
+            return
+        }
+        
+        encorder.endEncoding()
+        
+        if let drawable = view.currentDrawable {
+            cmdBuffer.present(drawable)
+        }
+        
+        cmdBuffer.commit()
+    }
+    
+    func setup(device: MTLDevice) {
+        self.commandQueue = device.makeCommandQueue()
     }
 }
